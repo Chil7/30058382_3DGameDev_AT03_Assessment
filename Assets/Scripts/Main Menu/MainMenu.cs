ad2847db541e640d21022dc34738720ff6e5bd1f
Chil7 <chil7br0h@gmail.com>
2023-05-25 15:13:44 +0800

    3.4 (Prototype, Script)
    Trying to Implement Main Menu

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -18,7 +18,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("DungeonScene");
     }
 
     public void QuitToDesktop()
