44206c34395fd5a456c6891935d052c61b410992
Chil7 <chil7br0h@gmail.com>
2023-06-09 08:42:41 +0800

    7.7 (Audio Settings)

--- Assets/Scripts/Settings/SettingManager.cs
+++ Assets/Scripts/Settings/SettingManager.cs
@@ -1,28 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class SettingManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    [SerializeField] Slider backgroundSlider;
+    [SerializeField] Slider sfxSlider;
+    [SerializeField] Slider masterSlider;
 
-    // Start is called before the first frame update
-    void Start()
+    const string MIXER_MASTER = "masterVolume";
+    const string MIXER_BGM = "backgroundVolume";
+    const string MIXER_SFX = "sfxVolume";
+
+    private void Awake()
     {
-        
+        backgroundSlider.onValueChanged.AddListener(SetBackgroundVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
     }
 
-    public void SetVolume (float volume)
+    public void SetMasterVolume (float volume)
     {
-        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat(MIXER_MASTER, Mathf.Log10(volume) * 20);
     }
-    public void SetMusicVolume (float volume)
+    public void SetBackgroundVolume (float volume)
     {
-        audioMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat(MIXER_BGM, Mathf.Log10(volume) * 20);
     }
     public void SetSFXVolume (float volume)
     {
-        audioMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat(MIXER_SFX, Mathf.Log10(volume) * 20);
+        //this will help the player to adjust the sound of SFX; basically a reference
+        FindObjectOfType<AudioManager>().Play("SwordSlash");
     }
 }
